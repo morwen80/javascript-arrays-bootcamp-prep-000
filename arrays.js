d442c8a8a8e0fd36c477bd1bb6d01759ec50b57e
Valeria Ragonese <ribesnero80@gmail.com>
2018-07-01 14:01:05 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -46,5 +46,5 @@ function accessElementInArray(array, index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   destructivelyRemoveElementFromBeginningOfArray.shift();
-  return destructivelyRemoveElementFromBeginningOfArray;
+
 }
