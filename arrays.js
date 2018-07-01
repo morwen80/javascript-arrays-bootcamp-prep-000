495ec654ea475e2dff23c0246fe2826ab1f0057d
Valeria Ragonese <ribesnero80@gmail.com>
2018-07-01 13:14:30 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,18 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element) {
+  const firstArray = [1];
+  
+    firstArray.unshift(element);
+    return newArray;
+      }
+
+// [element, ...chocolateBars];
+// return chocolateBars;
+//   }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+const newArray = [1];
+  newArray.unshift("foo");
+return newArray;
+    }
